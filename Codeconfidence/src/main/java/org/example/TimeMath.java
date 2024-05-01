3f16544d5a4149d45d4ded79bca1f4fd3d09998d
Matt Brown <matthewjbrown365@gmail.com>
2024-05-01 16:52:17 -0400

    started highcard game

--- Codeconfidence/src/main/java/org/example/TimeMath.java
+++ Codeconfidence/src/main/java/org/example/TimeMath.java
@@ -1,6 +1,11 @@
 package org.example;
 
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class TimeMath {
     /*
 Given two times in hours, minutes, and seconds (ie '15:04:24'), add or subtract them.
@@ -23,82 +28,98 @@ timeMath('01:24:31', '-', '02:31:41') === '22:52:50'
     }
 
 
-    public static String timeMath(String time1, String operator, String time2){
-        //break apart time1 into hr1, min1, and sec1
-        String[] time1Str = time1.split(":");
-        int hr1 = Integer.parseInt(time1Str[0]);
-        int min1 = Integer.parseInt(time1Str[1]);
-        int sec1 = Integer.parseInt(time1Str[2]);
-        //break apart time2 into hr2, min2, and sec2
-        String[] time2Str = time2.split(":");
-        int hr2 = Integer.parseInt(time2Str[0]);
-        int min2 = Integer.parseInt(time2Str[1]);
-        int sec2 = Integer.parseInt(time2Str[2]);
-        int totalHr = 0;
-        int totalMin = 0;
-        int totalSec = 0;
-        //check if operator is "+", then we add
-        if (operator.equals("+")){
-            totalHr = hr1 + hr2;
-            totalMin = min1 + min2;
-            totalSec = sec1 + sec2;
-        }
-        //add hrs together, mins together, and secs together
-        //else (operator is "-"), so subtract
-        else {
-                totalHr = hr1 - hr2;
-                totalMin = min1 - min2;
-                totalSec = sec1 - sec2;
-        }
-        //subtract hrs, then mins, then seconds
-        //then check to see if >= 60 in seconds
-        if (totalSec >=60){
-            //subtract 60 from seconds, and add 1 to minutes
-            totalSec -= 60;
-            totalMin++;
+    public static String timeMath(String time1, String operator, String time2) {
+
+        LocalTime time1Str = LocalTime.parse(time1);
+        LocalTime time2Str = LocalTime.parse(time2);
+
+        Duration time2Duration = Duration.between(LocalTime.MIN, time2Str);
+        LocalTime result = time1Str;
+
+        if (operator.equals("+")) {
+            result = result.plus(time2Duration);
+        } else if (operator.equals("-")) {
+            result = result.minus(time2Duration);
         }
 
-        //check to see if >= 60 in minutes
-        if (totalMin >= 60){
-            //subtract 60 in minutes, and add 1 to hours
-            totalMin -= 60;
-            totalHr++;
-        }
-
-        //check to see if > 24 in hours
-        //subtract 24 in hours
-        totalHr %= 24;
-        //check to see if hrs < 0
-
-        //check to see if seconds < 0
-        if (totalSec < 0){
-            //set seconds = 60 + seconds
-            totalSec += 60;
-            //subtract one from minutes
-            totalMin--;
-        }
-        //check to see if minutes < 0
-        if (totalMin < 0){
-            //set minutes = 60 + minutes
-            totalMin += 60;
-            //subtract one from hrs
-            totalHr--;
-        }
-        if (totalHr < 0){
-            //set hours = 24 + hours (which are negative)
-            totalHr += 24;
-        }
-
-
-        return padNumber(totalHr) + ":" + padNumber(totalMin) + ":" + padNumber(totalSec);
+        return result.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
     }
 
-    public static String padNumber(int num){
-        String totalNum = num + "";
-        if (totalNum.length() ==1){
-            totalNum = "0" + totalNum;
-        }
-        return totalNum;
-    }
+//        //break apart time1 into hr1, min1, and sec1
+//        String[] time1Str = time1.split(":");
+//        int hr1 = Integer.parseInt(time1Str[0]);
+//        int min1 = Integer.parseInt(time1Str[1]);
+//        int sec1 = Integer.parseInt(time1Str[2]);
+//        //break apart time2 into hr2, min2, and sec2
+//        String[] time2Str = time2.split(":");
+//        int hr2 = Integer.parseInt(time2Str[0]);
+//        int min2 = Integer.parseInt(time2Str[1]);
+//        int sec2 = Integer.parseInt(time2Str[2]);
+//        int totalHr = 0;
+//        int totalMin = 0;
+//        int totalSec = 0;
+//        //check if operator is "+", then we add
+//        if (operator.equals("+")){
+//            totalHr = hr1 + hr2;
+//            totalMin = min1 + min2;
+//            totalSec = sec1 + sec2;
+//        }
+//        //add hrs together, mins together, and secs together
+//        //else (operator is "-"), so subtract
+//        else {
+//                totalHr = hr1 - hr2;
+//                totalMin = min1 - min2;
+//                totalSec = sec1 - sec2;
+//        }
+//        //subtract hrs, then mins, then seconds
+//        //then check to see if >= 60 in seconds
+//        if (totalSec >=60){
+//            //subtract 60 from seconds, and add 1 to minutes
+//            totalSec -= 60;
+//            totalMin++;
+//        }
+//
+//        //check to see if >= 60 in minutes
+//        if (totalMin >= 60){
+//            //subtract 60 in minutes, and add 1 to hours
+//            totalMin -= 60;
+//            totalHr++;
+//        }
+//
+//        //check to see if > 24 in hours
+//        //subtract 24 in hours
+//        totalHr %= 24;
+//        //check to see if hrs < 0
+//
+//        //check to see if seconds < 0
+//        if (totalSec < 0){
+//            //set seconds = 60 + seconds
+//            totalSec += 60;
+//            //subtract one from minutes
+//            totalMin--;
+//        }
+//        //check to see if minutes < 0
+//        if (totalMin < 0){
+//            //set minutes = 60 + minutes
+//            totalMin += 60;
+//            //subtract one from hrs
+//            totalHr--;
+//        }
+//        if (totalHr < 0){
+//            //set hours = 24 + hours (which are negative)
+//            totalHr += 24;
+//        }
+//
+//
+//        return padNumber(totalHr) + ":" + padNumber(totalMin) + ":" + padNumber(totalSec);
+//    }
+//
+//    public static String padNumber(int num){
+//        String totalNum = num + "";
+//        if (totalNum.length() ==1){
+//            totalNum = "0" + totalNum;
+//        }
+//        return totalNum;
+//    }
 
 }
