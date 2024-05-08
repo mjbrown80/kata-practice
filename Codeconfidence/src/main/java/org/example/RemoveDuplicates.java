31fddac752ea97379b4537511916c2f0ff07df94
Matt Brown <matthewjbrown365@gmail.com>
2024-05-08 13:27:48 -0400

    kata4today

--- Codeconfidence/src/main/java/org/example/RemoveDuplicates.java
+++ Codeconfidence/src/main/java/org/example/RemoveDuplicates.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import java.util.List;
+import java.util.*;
 
 public class RemoveDuplicates {
 
@@ -11,22 +11,32 @@ public class RemoveDuplicates {
 //    removeDuplicates({4, 4, 4, 4, 8, 12, 16, 16}) → {4, 8, 12, 16}
 //    removeDuplicates({99, 88, 77, 777, 66, 66}) → {99, 88, 77, 777, 66}
 
-//    public static void main(String[] args) {
-//        // Tests
-//        List<Integer> list1 = new ArrayList<>(Arrays.asList(2, 2, 3, 4, 5));
-//        System.out.println("Expected: [2, 3, 4, 5] | Actual: " + removeDuplicates(list1));
-//
-//        List<Integer> list2 = new ArrayList<>(Arrays.asList(100, 201, 302));
-//        System.out.println("Expected: [100, 201, 302] | Actual: " + removeDuplicates(list2));
-//
-//        List<Integer> list3 = new ArrayList<>(Arrays.asList(8, 8, 8, 8, 1));
-//        System.out.println("Expected: [8, 1] | Actual: " + removeDuplicates(list3));
-//
-//    }
-//
-//    public static int removeDuplicates(List<Integer>){
-//
-//    }
-//
+    public static void main(String[] args) {
+        // Tests
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(2, 2, 3, 4, 5));
+        System.out.println("Expected: [2, 3, 4, 5] | Actual: " + removeDuplicates(list1));
 
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(100, 201, 302));
+        System.out.println("Expected: [100, 201, 302] | Actual: " + removeDuplicates(list2));
+
+        List<Integer> list3 = new ArrayList<>(Arrays.asList(8, 8, 8, 8, 1));
+        System.out.println("Expected: [8, 1] | Actual: " + removeDuplicates(list3));
+
+    }
+
+    public static List<Integer> removeDuplicates(List<Integer> nums){
+        List<Integer> revised = new ArrayList<>();
+        for (Integer num : nums){
+            if (!revised.contains(num)){
+                revised.add(num);
+            }
+        }
+        return revised;
+    }
+
+//    public static List<Integer> removeDuplicates(List<Integer> nums){
+//        Set<Integer> revised = new LinkedHashSet<>(nums);
+//
+//        return new ArrayList<Integer>(revised);
+//    }
 }
