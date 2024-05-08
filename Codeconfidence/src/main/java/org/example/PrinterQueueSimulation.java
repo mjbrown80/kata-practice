73cd171746fc4f0c6d2f23c065a810aadd650db3
Matt Brown <matthewjbrown365@gmail.com>
2024-05-08 12:19:27 -0400

    another

--- Codeconfidence/src/main/java/org/example/PrinterQueueSimulation.java
+++ Codeconfidence/src/main/java/org/example/PrinterQueueSimulation.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class PrinterQueueSimulation {
 //    You are tasked with writing a method called printerQueueSimulation/PrinterQueueSimulation
 //    that simulates a printer queue for a busy office.
@@ -18,21 +23,37 @@ public class PrinterQueueSimulation {
 //   9. Print a message indicating the total number of printed documents.
 //   10. Return the total number of printed documents.
 
-//    public static void main(String[] args) {
-//        // Tests
-//        List<String> documentsList2 = new ArrayList<>(List.of("Candidate Resume", "Candidate Cover Letter", "Candidate References"));
-//        System.out.println("TEST: Expected: 3 | Actual: " + printerQueueSimulation(documentsList2));
-//
-//        System.out.println("-------------------");
-//
-//        List<String> documentsList1 = new ArrayList<>(List.of("Document 1", "Document 2", "Document 3", "Document 4", "Document 5"));
-//        System.out.println("TEST: Expected: 5 | Actual: " + printerQueueSimulation(documentsList1));
-//
-//        System.out.println("-------------------");
-//
-//        List<String> documentsList3 = new ArrayList<>(List.of("Visit PocketLintForCash.com!", "Win a Free Vacation to Hawaii!", "12 Things Your Cat Does While You're At Work!", "Spam! Spam! Spam!"));
-//        System.out.println("TEST: Expected: 4 | Actual: " + printerQueueSimulation(documentsList3));
-//
-//    }
+    public static void main(String[] args) {
+        // Tests
+        List<String> documentsList2 = new ArrayList<>(List.of("Candidate Resume", "Candidate Cover Letter", "Candidate References"));
+        System.out.println("TEST: Expected: 3 | Actual: " + printerQueueSimulation(documentsList2));
+
+        System.out.println("-------------------");
+
+        List<String> documentsList1 = new ArrayList<>(List.of("Document 1", "Document 2", "Document 3", "Document 4", "Document 5"));
+        System.out.println("TEST: Expected: 5 | Actual: " + printerQueueSimulation(documentsList1));
+
+        System.out.println("-------------------");
+
+        List<String> documentsList3 = new ArrayList<>(List.of("Visit PocketLintForCash.com!", "Win a Free Vacation to Hawaii!", "12 Things Your Cat Does While You're At Work!", "Spam! Spam! Spam!"));
+        System.out.println("TEST: Expected: 4 | Actual: " + printerQueueSimulation(documentsList3));
+
+    }
+    public static int printerQueueSimulation(List<String> documents){
+        Queue<String> queue = new LinkedList<>();
+        for (String document : documents){
+            queue.offer(document);
+            System.out.println("==> '" + document + "' arrived and joined the printer queue");
+        }
+        int totalPrintedDocuments = 0;
+        while(!queue.isEmpty()){
+            String document = queue.poll();
+            System.out.println("... Printer printing '" + document + "' ...");
+            System.out.println("Document finished printing ");
+            totalPrintedDocuments++;
+        }
+        System.out.println("**** Total Documents printed: " + totalPrintedDocuments + " ****");
+        return totalPrintedDocuments;
+    }
 
 }
--- Codeconfidence/src/main/java/org/example/ReturnMost.java
+++ Codeconfidence/src/main/java/org/example/ReturnMost.java
@@ -1,20 +1,60 @@
 package org.example;
 
+import java.awt.image.ImageProducer;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ReturnMost {
 //    Write a method called returnMost/ReturnMost that takes in a
 //    string and returns the character that appears the most in the string
 //
 //    EX returnMost("Hi, this is my string")  => returns i
 
-//    public static void main(String[] args) {
-//        char i = returnMost("Hi, this is my string");
-//        System.out.println("'i' is expected - actual is " + i);
-//        i = returnMost("HiHihihithere");
-//        System.out.println("'h' is expected - actual is " + i);
-//    }
-//
-//    public static String returnMost(String str){
-//
-//    }
+    public static void main(String[] args) {
+        char i = returnMost("Hi, this is my string");
+        System.out.println("'i' is expected - actual is " + i);
+        i = returnMost("HiHihihithere");
+        System.out.println("'h' is expected - actual is " + i);
+    }
+
+    public static char returnMost(String str){
+        //create Map <Character,int>
+        Map<Character, Integer> letter = new HashMap<>();
+        //loop through str
+        for (int i = 0; i < str.length(); i++){
+            //make sure we have an alpha char
+            char c = str.charAt(i);
+            //convert to lower case
+            char lowerC = Character.toLowerCase(c);
+            //if map doesn't contain char
+            if (c != ' '){
+                if (!letter.containsKey(lowerC)){
+                    //add char to map with count of 1
+                    letter.put(lowerC,1);
+                }else{
+                    //get count, increase by 1 , put char and count back in map
+                    int count = letter.get(lowerC);
+                    letter.put(lowerC, ++count);
+                }
+            }
+        }
+        //then determine who has the most
+        //int most = 0;
+        int most = 0;
+        //String mostChar = '';
+        char mostChar = '\0';
+        //loop through the map
+        for (Map.Entry<Character, Integer> entry: letter.entrySet()){
+            //if(most < count)
+            if (letter.get(entry.getKey()) > most){
+                //most = count
+                most = entry.getValue();
+                //mostChar = key
+                mostChar = entry.getKey();
+            }
+        }
+        //return mostChar
+        return mostChar;
+    }
 
 }
