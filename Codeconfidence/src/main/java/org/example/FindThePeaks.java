259da025e3de6f826a5cc13228163b8827cc9dfd
Matt Brown <matthewjbrown365@gmail.com>
2024-06-08 21:07:41 -0400

    inital findthepeaks

---
+++ Codeconfidence/src/main/java/org/example/FindThePeaks.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindThePeaks {
+
+    public static void main(String[] args){
+
+
+    }
+    public static List<Integer> findPeaks(int[] mountain){
+
+        List<Integer> peaks = new ArrayList<>();
+
+        return peaks;
+    }
+}
