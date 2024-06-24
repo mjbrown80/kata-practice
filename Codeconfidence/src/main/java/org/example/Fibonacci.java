e76497f2c4a999585862b3941813b4f8a478d1e9
Matt Brown <matthewjbrown365@gmail.com>
2024-06-24 13:52:57 -0400

    fizzbuzz

---
+++ Codeconfidence/src/main/java/org/example/Fibonacci.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Fibonacci {
+//    Create a method called fibonacci with no parameters.
+//    In a Fibonacci sequence, every number after the first two is the sum of the two preceding ones.
+//    Return an array of integers containing the Fibonacci sequence of 0, 1, 1, 2, 3, and so on for the values less than 2000.
+//
+//    For example:
+//
+//    fibonacci() → {1, 1, 2, 3, 5, 8, 13, ... 987, 1597}
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ Codeconfidence/src/main/java/org/example/PrimeFactors.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class PrimeFactors {
+//    Create a method called primeFactors that takes in an integer n.
+//    Return an integer array of the [prime factors] of n(https://www.mathsisfun.com/definitions/prime-factor.html).
+//    Prime factors are the numbers you can multiply to get n that you can't break down into any smaller factors.
+//    You can assume the input is greater than 1.
+//
+//            For example:
+//
+//            primeFactors(6) → {2, 3}
+//    primeFactors(28) → {2, 2, 7}
+//    primeFactors(667) → {23, 29}
+
+    public static void main(String[] args){
+
+    }
+}
